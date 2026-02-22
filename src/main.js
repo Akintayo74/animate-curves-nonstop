b5f1774d006548062967ea788791466b7646874a
Akintayo Akinnibosun <akinnibosunakintayo74@gmail.com>
2026-02-22 01:15:44 +0100

    Create animation function and map normalized values to svg

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import { normalize } from './utils.js';
+
 //Step 1 -- Create an Audio Context
 let audioContext = null;
 let analyser = null;
+let dataArray = null;
 
 const playButton = document.getElementById("playButton");
 const audioElement = document.querySelector("audio");
+const svgPath = document.querySelector("path");
 
 
 playButton.addEventListener("click", function () {
@@ -13,8 +17,14 @@ playButton.addEventListener("click", function () {
     const source = audioContext.createMediaElementSource(audioElement);
     analyser = audioContext.createAnalyser();
     analyser.fftSize = 256;
+    
+    const bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+    
     source.connect(analyser);
-    analyser.connect(audioContext.destination)
+    analyser.connect(audioContext.destination);
+    
+    animate();
   } else if (audioContext.state === "suspended") {
     audioContext.resume();
   }
@@ -27,4 +37,22 @@ playButton.addEventListener("click", function () {
     audioElement.pause();
     playButton.dataset.playing = "false";
   }
+
 });
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  
+  analyser.getByteFrequencyData(dataArray);
+  
+  const bassArray = dataArray.slice(0, 10);
+  
+  const bassSum = bassArray.reduce((total, currentNumber) => total + currentNumber, 0);
+  
+  const bassLevel = bassSum / 10;
+  
+  const normalizedBassLevel = normalize(bassLevel, 0, 255, 20, 0);
+  
+  svgPath.setAttribute('d', `M 11, 15 Q 6, ${normalizedBassLevel} 3, 15`)
+}
